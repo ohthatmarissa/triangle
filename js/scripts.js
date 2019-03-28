1fa4cfa669ccd06c9b4d274e013378d11af8db86
Maya Frame and Marissa Perry <marissa@>
2019-03-28 16:29:21 -0700

    add working equasions for all triangles

--- js/scripts.js
+++ js/scripts.js
@@ -9,12 +9,20 @@ $(document).ready(function(){
   if(side1 && side2 && side3) {
     if(side1 === side2 && side2 === side3) {
       $('#output').val('equilateral');
+  } else if(side1 === side2 || side1 === side3 || side2 === side3) {
+      $('#output').val('Isosceles');
+  } else {
+    if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1){
+      $('#output').val('Scalene');
+    } else {
+      $('#output').val('not a triangle');
+    }
   }
-}
-  if
+  } else {
+    alert('Please enter value for all sides!')
 
+  }
     $("#output").text("your triangle is " + $('#output').val() + "!");
-
 
 });
 });
