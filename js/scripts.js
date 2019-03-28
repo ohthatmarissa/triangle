f68644b2879c810656b930aa855063e2f036d004
Maya Frame and Marissa Perry <maya@>
2019-03-28 15:16:59 -0700

    initial form structure, js var set

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function)) {
+  $('form#triangle').submit(function(event) {
+    event.preventDefault();
+  }
+  var side1 = parseInt($('#side1').val());
+  var side2 = parseInt($('#side2').val());
+  var side3 = parseInt($('#side3').val());
+  $('#output').text(result);
+
+if(side1 && side2 && side3) {
+  if(side1 === side2 && side2 === side3) {
+    #
+  }
+}
+});
